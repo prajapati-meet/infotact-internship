8f062c4621d78663187e3c91b66e825211de2595
Thirumaleswari <tirumaleswarip16@gmail.com>
2026-06-07 10:50:07 +0530

    Added LoginRequest DTO and resolved walletRepository merge conflict

---
+++ Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System/src/main/java/com/payment/ledger/dto/request/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.payment.ledger.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequest {
+	
+	
+	@NotBlank(message ="Email is required")
+	@Email(message ="please enter a valid email address")
+	
+	private String email;
+	private String password;
+	
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
--- Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System/src/main/java/com/payment/ledger/repository/WalletRepository.java
+++ Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System/src/main/java/com/payment/ledger/repository/WalletRepository.java
@@ -1,5 +1,6 @@
 package com.payment.ledger.repository;
 
+
 import com.payment.ledger.entity.User;
 import com.payment.ledger.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface WalletRepository extends JpaRepository<Wallet,UUID>{
-	
-	optional<Wallet> findByUser(User user);
-	optional<Wallet> findByUserId(UUID userId);
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    Optional<Wallet> findByUser(User user);
+
+    Optional<Wallet> findByUserId(UUID userId);
 
 }
