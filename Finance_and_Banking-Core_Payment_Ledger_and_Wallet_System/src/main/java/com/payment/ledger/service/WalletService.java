6b0b5e538530ea276163a9ada896ed9a33a46606
Thirumaleswari <tirumaleswarip16@gmail.com>
2026-06-09 13:00:17 +0530

    Implemented walletController endpoint

---
+++ Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System/src/main/java/com/payment/ledger/service/WalletService.java
@@ -0,0 +1,15 @@
+package com.payment.ledger.service;
+
+import com.payment.ledger.dto.response.WalletResponse;
+import com.payment.ledger.entity.User;
+
+public class WalletService {
+
+	public WalletResponse getWalletForUser(User user) {
+	
+		return null;
+	}
+	
+	
+
+}
